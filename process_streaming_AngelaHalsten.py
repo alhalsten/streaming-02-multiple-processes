47dfe41a7a9da413746bcd94ea4d46286edb595b
S553705@nwmissouri.edu <alhalsten@gmail.com>
2023-09-01 23:31:23 -0500

    Created out9.txt, process_streaming_AngelaHalsten.

--- process_streaming_AngelaHalsten.py
+++ process_streaming_AngelaHalsten.py
@@ -17,6 +17,7 @@
 
 # Import from Python Standard Library
 
+import os
 import csv
 import socket
 import time
@@ -34,17 +35,30 @@ logging.basicConfig(
 HOST = "localhost"
 PORT = 9999
 ADDRESS_TUPLE = (HOST, PORT)
-INPUT_FILE_NAME = "batchfile_0_farenheit.csv"
+INPUT_FILE_NAME= "Basic_Stats.csv"
+report_files = [
+    "Out9.txt",
+]
+
+def delete_report_files():
+    """
+    Delete the report files from the current directory.
+    """
+    for file in report_files:
+        try:
+            os.remove(file)
+        except FileNotFoundError:
+            pass
 
 # Define program functions (bits of reusable code)
 
 
 def prepare_message_from_row(row):
     """Prepare a binary message from a given row."""
-    Year, Month, Day, Time, TempF = row
+    Age, BirthPlace, Birthday, College, CurrentStatus, CurrentTeam,Experience, Height, HighSchool, HighSchoolLocation, Name, Number, PlayerID, Position, Weight, YearsPlayed = row
     # use an fstring to create a message from our data
     # notice the f before the opening quote for our string?
-    fstring_message = f"[{Year}, {Month}, {Day}, {Time}, {TempF}]"
+    fstring_message = f"[{Age}, {BirthPlace}, {Birthday}, {College}, {CurrentStatus}, {CurrentTeam}, {Experience}, {Height}, {HighSchool}, {HighSchoolLocation}, {Name}, {Number}, {PlayerID}, {Position}, {Weight}, {YearsPlayed}]"
 
     # prepare a binary (1s and 0s) message to stream
     MESSAGE = fstring_message.encode()
@@ -82,18 +96,16 @@ def stream_row(input_file_name, address_tuple):
             MESSAGE = prepare_message_from_row(row)
             sock_object.sendto(MESSAGE, address_tuple)
             logging.info(f"Sent: {MESSAGE} on port {PORT}. Hit CTRL-c to stop.")
-            time.sleep(3) # wait 3 seconds between messages
+            time.sleep(2) # wait 2 seconds between messages
 
 # ---------------------------------------------------------------------------
 # If this is the script we are running, then call some functions and execute code!
 # ---------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    try:
-        logging.info("===============================================")
-        logging.info("Starting fake streaming process.")
-        stream_row(INPUT_FILE_NAME, ADDRESS_TUPLE)
-        logging.info("Streaming complete!")
-        logging.info("===============================================")
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
+    delete_report_files()
+
+    if not stream_row(INPUT_FILE_NAME, ADDRESS_TUPLE):
+        exit(1)
+    
+   
